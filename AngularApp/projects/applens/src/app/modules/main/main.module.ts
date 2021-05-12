9b03694435a8b6466dcc966f13fc792e35646eac
XiaoxuHU <xiaoxhu@microsoft.com>
2021-05-12 09:50:32 -0700

    Update Applens UI

--- AngularApp/projects/applens/src/app/modules/main/main.module.ts
+++ AngularApp/projects/applens/src/app/modules/main/main.module.ts
@@ -6,7 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
 import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
 import { CUSTOM_MOMENT_FORMATS } from '../../shared/models/datetime';
-import { FabDialogModule, FabButtonModule, FabTextFieldModule, FabCalloutModule } from '@angular-react/fabric';
+import { FabDialogModule, FabButtonModule, FabTextFieldModule, FabCalloutModule, FabChoiceGroupModule, FabIconModule, FabDropdownModule } from '@angular-react/fabric';
+import { DiagnosticDataModule } from 'diagnostic-data';
 import { SharedModule } from '../../shared/shared.module';
 
 export const MainModuleRoutes : ModuleWithProviders = RouterModule.forChild([
@@ -28,7 +29,12 @@ export const MainModuleRoutes : ModuleWithProviders = RouterModule.forChild([
     FabButtonModule, 
     FabDialogModule,
     FabTextFieldModule,
-    FabCalloutModule
+    FabCalloutModule,
+    FabChoiceGroupModule,
+    FabIconModule,
+    FabDropdownModule,
+    DiagnosticDataModule,
+    SharedModule
   ],
   providers: [{
     provide: OWL_DATE_TIME_FORMATS, 
