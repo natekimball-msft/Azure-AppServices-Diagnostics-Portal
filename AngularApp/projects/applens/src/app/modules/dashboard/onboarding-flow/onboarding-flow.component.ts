30632e815d6098a37f017105163a6e9ddb1006b0
XiaoxuHU <xiaoxhu@microsoft.com>
2021-11-05 08:44:29 -0700

    Update publish notification style

--- AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
@@ -229,7 +229,7 @@ export class OnboardingFlowComponent implements OnInit {
 
   submittedPanelStyles: IPanelProps["styles"] = {
     root: {
-      height: "70px",
+      height: "100px",
     },
     content: {
       padding: "0px"
