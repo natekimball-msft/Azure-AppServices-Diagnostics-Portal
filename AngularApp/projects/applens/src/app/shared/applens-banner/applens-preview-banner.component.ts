19ace0d731049103586451a1047e8e56b09424c5
XiaoxuHU <xiaoxhu@microsoft.com>
2021-11-10 12:45:07 -0800

    Modify preview banner in Applens preview

--- AngularApp/projects/applens/src/app/shared/applens-banner/applens-preview-banner.component.ts
+++ AngularApp/projects/applens/src/app/shared/applens-banner/applens-preview-banner.component.ts
@@ -6,28 +6,37 @@ import { TelemetryService } from 'diagnostic-data';
   templateUrl: './applens-preview-banner.component.html',
   styleUrls: ['./applens-preview-banner.component.scss']
 })
-export class ApplensBannerComponent implements OnInit{
+export class ApplensBannerComponent implements OnInit {
   public showBanner: boolean = true;
   private url: URL = new URL(window.location.href);
   private readonly prodHostName = "applens.azurewebsites.net";
   private readonly previewHostName = "applens-preview.azurewebsites.net";
-  public isPreview: boolean = false;
-  constructor(private _telemetryService:TelemetryService) { }
+  public isPreview: boolean = true;
+  constructor(private _telemetryService: TelemetryService) { }
 
   ngOnInit() {
   }
 
-  switchView(event:Event) {
+  switchView(event: Event) {
     event.stopPropagation();
     const url = this.url;
     this._telemetryService.logEvent("SwitchApplensSlot", {
-      currentSlot: this.isPreview ? "Preview": "Prod",
+      currentSlot: this.isPreview ? "Preview" : "Prod",
       url: window.location.href
     });
 
     url.hostname = this.isPreview ? this.prodHostName : this.previewHostName;
     url.port = "";
     window.location.href = url.href;
+  }
+
+  emailToApplensTeam(event: Event) {
+    event.stopPropagation();
+
+    const body = encodeURIComponent('Current site: ' + window.location.href + '\n' + 'Please provide feedback here:');
+    const subject = `Feedback for AppLens VNext`
+    const link = `mailto:applensv2@microsoft.com?subject=${subject}&body=${body}`;
+    window.open(link);
   }
 
 }
