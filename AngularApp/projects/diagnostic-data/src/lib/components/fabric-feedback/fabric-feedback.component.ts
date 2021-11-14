86ec47c0ce4e0252c3e56bd75909d0235c3f6e43
Cindy Peng <38216903+cindylovescoding@users.noreply.github.com>
2021-11-13 16:30:48 -0800

    Add theming/high contrast mode for Diagnostic and Solve blade (#1335)
    * Add load theme method
    
    * Adding two built in theming
    
    * Resolve conflicts
    
    * Add Theme service
    
    * Add more css variables
    
    * Add high contrast theming
    
    * Fix card container and load view for new detector view
    
    * Add generic theming service and highchart theming
    
    * search box bug fixing
    
    * Fix theme service bug for 0 highcontrast value
    
    * Add property getter method for component access
    
    * Fix auto heal style

--- AngularApp/projects/diagnostic-data/src/lib/components/fabric-feedback/fabric-feedback.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/fabric-feedback/fabric-feedback.component.ts
@@ -124,7 +124,6 @@ export class FabricFeedbackComponent implements AfterViewInit, OnInit, OnDestroy
     this.globals.openFeedback = false;
   }
 
-
   dismissedFeedbackHandler() {
     if (this.feedbackPanelConfig.notResetOnDismissed) {
       this.globals.openFeedback = false;
