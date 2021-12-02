d0c6fb4091427e99182c3d0515253f97a61309b0
Cindy Peng <38216903+cindylovescoding@users.noreply.github.com>
2021-12-02 13:32:59 -0800

    Reduce webpack bundle size and unused components (#1358)
    * Refactor detector-time-picker
    
    * Fix for enable new detector UI in D&S blade
    
    * Remove nvd3 and datatable
    
    * use CDN for javascripts
    
    * Change CDN name
    
    * Reduce moment library size and fix time range
    
    * Resolve merge conflicts
    
    * Resolving merging conflict
    
    * Resolving merging conflict
    
    Co-authored-by: XiaoxuHU <xiaoxhu@microsoft.com>

--- AngularApp/projects/app-service-diagnostics/src/app/availability/availability.module.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/availability/availability.module.ts
@@ -3,20 +3,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { SolutionsModule } from '../solutions/solutions.module';
 import { DiagnosticDataModule } from 'diagnostic-data';
-
 import { AvailabilityComponent } from './availability.component';
 import { AppCurrentHealthComponent } from './currenthealth/app-current-health.component';
-import { AvailabilityGraphComponent } from './analysis/availability-graph.component';
-import { ObservationsComponent } from './observations/observations.component';
-import { ObservationsAvailabilityComponent } from './observations/observations-availability.component';
-import { ObservationsPerformanceComponent } from './observations/observations-performance.component';
 import { SolutionListComponent } from './solutions/solution-list.component';
-import { AppAnalysisComponent } from './analysis/app-analysis.component';
-import { PerfAnalysisComponent } from './analysis/perf-analysis.component';
-import { WebAppRestartComponent } from './analysis/webapprestart/webapprestart.component';
-import { MemoryAnalysisComponent } from './analysis/memory-analysis/memory-analysis.component';
 import { DetectorViewBaseComponent } from './detector-view/detector-view-base/detector-view-base.component';
-import { DetectorViewMainComponent } from './detector-view/detector-view-main/detector-view-main.component';
 import { DetectorViewProblemComponent } from './detector-view/detector-view-problem/detector-view-problem.component';
 import { DetectorViewInstanceDetailComponent } from './detector-view/detector-view-instance-detail/detector-view-instance-detail.component';
 import { SiteCpuAnalysisDetectorComponent } from './detector-view/detectors/site-cpu-analysis-detector/site-cpu-analysis-detector.component';
@@ -33,8 +23,6 @@ import { AspNetCoreComponent } from "./detector-view/detectors/aspnetcore-detect
 import { AppInsightsTileComponent } from './app-insights/app-insights-tile.component';
 import { AppInsightsExceptionsComponent } from './app-insights/exceptions/app-insights-exceptions.component';
 import { AppInsightsDependenciesComponent } from './app-insights/dependencies/app-insights-dependencies.component';
-import { TcpConnectionsAnalysisComponent } from './analysis/tcpconnectionsanalysis/tcp-connections-analysis.component';
-import { ProblemSolutionComponent } from './problem-solution/problem-solution.component';
 import { DetectorLoaderComponent } from './detector-view/detector-loader/detector-loader.component';
 import { AutohealingDetectorComponent } from './detector-view/detectors/autohealing-detector/autohealing-detector.component';
 import { RerouteResolver } from './reroute/reroute.resolver';
@@ -43,16 +31,8 @@ import { RerouteResolver } from './reroute/reroute.resolver';
     declarations: [
         AvailabilityComponent,
         DetectorViewBaseComponent,
-        DetectorViewMainComponent,
         AppCurrentHealthComponent,
-        AvailabilityGraphComponent,
-        ObservationsComponent,
-        ObservationsAvailabilityComponent,
-        ObservationsPerformanceComponent,
         SolutionListComponent,
-        AppAnalysisComponent,
-        PerfAnalysisComponent,
-        WebAppRestartComponent,
         ToolsMenuComponent,
         DetectorViewInstanceDetailComponent,
         DetectorViewProblemComponent,
@@ -62,15 +42,12 @@ import { RerouteResolver } from './reroute/reroute.resolver';
         FrebAnalysisDetectorComponent,
         PhpLogAnalyzerComponent,
         DockerContainerIntializationComponent,
-        MemoryAnalysisComponent,
         CommittedMemoryUsageComponent,
         PageFileOperationsComponent,
         AspNetCoreComponent,
-        TcpConnectionsAnalysisComponent,
         AppInsightsTileComponent,
         AppInsightsExceptionsComponent,
         AppInsightsDependenciesComponent,
-        ProblemSolutionComponent,
         DetectorLoaderComponent,
         AutohealingDetectorComponent,
     ],
@@ -81,12 +58,8 @@ import { RerouteResolver } from './reroute/reroute.resolver';
         DiagnosticDataModule
     ],
     exports: [
-        ObservationsComponent,
-        ObservationsAvailabilityComponent,
-        ObservationsPerformanceComponent,
         DetectorViewProblemComponent,
         AppInsightsTileComponent,
-        ProblemSolutionComponent,
         AutohealingDetectorComponent,
         DetectorLoaderComponent
     ],
