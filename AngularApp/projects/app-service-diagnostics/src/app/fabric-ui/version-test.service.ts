648ca8416513e19d6a4c3615270cf2f38c264a19
Yun Jung Choi <49920477+yunjchoi@users.noreply.github.com>
2021-12-15 13:06:40 -0800

    Enable genie for batch (#1378)
    * Update config.json
    
    Updating the api version & overview detector id
    
    * Update version-test.service.ts
    
    Routing Batch traffic to Genie v3
    
    * Add files via upload
    
    * Update config.json
    
    Updating the category id to match the icon naming convention

--- AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
@@ -19,7 +19,8 @@ export const allowV3PResourceTypeList: { type: string, allowSwitchBack: boolean 
     { type: "microsoft.containerservice/managedclusters", allowSwitchBack: false },
     { type: "microsoft.web/containerapps", allowSwitchBack: false },
     { type: "microsoft.servicefabric/clusters", allowSwitchBack: false },
-    { type: "microsoft.resourceconnector/appliances", allowSwitchBack: false }
+    { type: "microsoft.resourceconnector/appliances", allowSwitchBack: false },
+    { type: "microsoft.batch/batchaccounts/", allowSwitchBack: false }
 ];
 
 
