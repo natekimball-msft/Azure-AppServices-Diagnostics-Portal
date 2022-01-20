5ae540d0e661f5ab542482409219c5e1e1c1f5a0
Hawk Foreste <hawfor@microsoft.com>
2022-01-20 12:07:53 -0800

    Add nullable class to support ncloud (#1398)
    * Add nullable services so that asp.net core dependency injection will not fail for national clouds.
    
    * Add new nullable service.

---
+++ ApplensBackend/Services/ResourceConfigService/NullableResourceConfigService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppLensV3.Models;
+
+namespace AppLensV3.Services
+{
+    public class NullableResourceConfigService : IResourceConfigService
+    {
+        public Task<ResourceConfig> GetResourceConfig(string resourceType)
+        {
+            return null;
+        }
+    }
+}
