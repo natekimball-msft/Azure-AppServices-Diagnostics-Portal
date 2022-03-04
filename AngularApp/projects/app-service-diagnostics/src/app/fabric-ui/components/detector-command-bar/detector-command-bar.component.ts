57e3950d483ea98bb95b16ef994ff81e8566d4a2
Shekhar Gupta <shgup@microsoft.com>
2022-03-04 12:38:00 -0800

    Update 10% of subs to see DownloadReport Button (#1464)
    the formula we are using to calculate this exposure works only for minimum 6% of the subs and not below that.

--- AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/components/detector-command-bar/detector-command-bar.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/components/detector-command-bar/detector-command-bar.component.ts
@@ -50,8 +50,8 @@ export class DetectorCommandBarComponent implements AfterViewInit {
 
   ngOnInit(): void {
     let subscriptionId = this._route.parent.snapshot.params['subscriptionid'];
-    // add logic for presenting initially to only 1% of Subscriptions:  percentageToRelease = 0.01% (1=100%)
-    let percentageToRelease = 0.01;
+    // add logic for presenting initially to only 10% of Subscriptions
+    let percentageToRelease = 0.1;
     // roughly split of percentageToRelease of subscriptions to use new feature.
     let firstDigit = "0x" + subscriptionId.substr(0, 1);
     this.displayRPDFButton = (16 - parseInt(firstDigit, 16)) / 16 <= percentageToRelease && this._checkIsWindowsApp();
