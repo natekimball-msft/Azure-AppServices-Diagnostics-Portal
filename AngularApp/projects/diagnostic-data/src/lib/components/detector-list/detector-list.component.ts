f498671bb6e87e0dc9e98ca8ec6dca14ed58baef
Darrel Donald <darreldonald@microsoft.com>
2022-03-25 15:57:06 -0700

    updates to view detector button

--- AngularApp/projects/diagnostic-data/src/lib/components/detector-list/detector-list.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/detector-list/detector-list.component.ts
@@ -441,10 +441,6 @@ export class DetectorListComponent extends DataRenderBaseComponent {
           if (this.isPublic) {
             const url = this._router.url.split("?")[0];
             const routeUrl = url.endsWith("/overview") ? `../detectors/${targetDetector}` : `../../detectors/${targetDetector}`;
-            // this._router.navigate([routeUrl], {
-            //   queryParams: this.queryParams,
-            //   relativeTo: this._activatedRoute
-            // });
             let paramString = "";
             Object.keys(this.queryParams).forEach(x => {
               paramString = paramString === "" ? `${paramString}${x}=${this.queryParams[x]}` : `${paramString}&${x}=${this.queryParams[x]}`;
