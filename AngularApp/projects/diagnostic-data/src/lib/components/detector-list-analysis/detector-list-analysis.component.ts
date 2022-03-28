f55156be9d1583c0b19405a098320d3d43e5aff3
Darrel Donald <darreldonald@microsoft.com>
2022-03-28 16:00:08 -0700

    Darreldonald/viewdetails button (#1505)
    * update view details button
    
    * updates to view detector button
    
    * updates to details links
    
    * remove unused code
    
    * remove resourceservice
    
    * remove resourceservice

--- AngularApp/projects/diagnostic-data/src/lib/components/detector-list-analysis/detector-list-analysis.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/detector-list-analysis/detector-list-analysis.component.ts
@@ -31,8 +31,6 @@ import * as momentNs from 'moment';
 const moment = momentNs;
 import { ILinkProps, PanelType } from 'office-ui-fabric-react';
 import { GenericBreadcrumbService } from '../../services/generic-breadcrumb.service';
-import { UriUtilities } from '../../utilities/uri-utilities';
-import { ResourceService } from 'projects/applens/src/app/shared/services/resource.service';
 
 const WAIT_TIME_IN_SECONDS_TO_ALLOW_DOWNTIME_INTERACTION: number = 58;
 const PERCENT_CHILD_DETECTORS_COMPLETED_TO_ALLOW_DOWNTIME_INTERACTION: number = 0.9;
@@ -900,66 +898,19 @@ export class DetectorListAnalysisComponent extends DataRenderBaseComponent imple
           padding: '10px'
         }
     }
-    linkAddress: ILinkProps['href'] = "";
-    linkTarget: ILinkProps['target'] = "_blank";
 
     public selectDetectorNewTab(viewModel: any) {
         if (viewModel != null && viewModel.model.metadata.id) {
             let detectorId = viewModel.model.metadata.id;
-            let categoryName = "";
-
-            if (viewModel.model.metadata.category) {
-                categoryName = viewModel.model.metadata.category.replace(/\s/g, '');
-            }
-            else {
-                // For uncategorized detectors:
-                // If it is home page, redirect to availability category. Otherwise stay in the current category page.
-                categoryName = this._router.url.split('/')[11] ? this._router.url.split('/')[11] : "availabilityandperformance";
-            }
 
             if (detectorId !== "") {
-                const clickDetectorEventProperties = {
-                    'ChildDetectorName': viewModel.model.title,
-                    'ChildDetectorId': viewModel.model.metadata.id,
-                    'IsExpanded': true,
-                    'Status': viewModel.model.status,
-                    'SearchMode': this.searchMode
-                };
+                let paramString = "";
+                Object.keys(this.queryParams).forEach(x => {
+                    paramString = paramString === "" ? `${paramString}${x}=${this.queryParams[x]}` : `${paramString}&${x}=${this.queryParams[x]}`;
+                });
+                const linkAddress = `${this._router.url.split('/analysis/')[0]}/detectors/${detectorId}?${paramString}`;
 
-                // Log children detectors click
-                this.logEvent(TelemetryEventNames.ChildDetectorClicked, clickDetectorEventProperties);
-                this.queryParams = UriUtilities.removeChildDetectorStartAndEndTime(this._activatedRoute.snapshot.queryParams);
-
-                if (detectorId === 'appchanges' && !this._detectorControl.internalClient) {
-                    this.portalActionService.openChangeAnalysisBlade(this._detectorControl.startTimeString, this._detectorControl.endTimeString);
-                } else {
-                    //TODO, For D&S blade, need to add a service to find category and navigate to detector
-                    if (viewModel.model.startTime != null && viewModel.model.endTime != null) {
-
-                        this._detectorControl.setCustomStartEnd(viewModel.model.startTime, viewModel.model.endTime);
-                        //Todo, detector control service should able to read and infer TimePickerOptions from startTime and endTime
-                        this._detectorControl.updateTimePickerInfo({
-                            selectedKey: TimePickerOptions.Custom,
-                            selectedText: TimePickerOptions.Custom,
-                            startDate: new Date(viewModel.model.startTime),
-                            endDate: new Date(viewModel.model.endTime)
-                        });
-                        this.updateBreadcrumb();
-                        let paramString = "";
-                        Object.keys(this.queryParams).forEach(x => {
-                            paramString = paramString === "" ? `${paramString}${x}=${this.queryParams[x]}` : `${paramString}&${x}=${this.queryParams[x]}`;
-                        });
-                        this.linkAddress = `${this._router.url.split('/analysis/')[0]}/detectors/${detectorId}?${paramString}`;
-                    }
-                    else {
-                        this.updateBreadcrumb();
-                        let paramString = "";
-                        Object.keys(this.queryParams).forEach(x => {
-                            paramString = paramString === "" ? `${paramString}${x}=${this.queryParams[x]}` : `${paramString}&${x}=${this.queryParams[x]}`;
-                        });
-                        this.linkAddress = `${this._router.url.split('/analysis/')[0]}/detectors/${detectorId}?${paramString}`;
-                    }
-                }
+                window.open(linkAddress, '_blank');
             }
         }
     }
