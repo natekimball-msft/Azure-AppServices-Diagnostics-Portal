d5706d30d02b31e14ead89c9b37d7d15c8825bc2
nmallick1 <nmallick@microsoft.com>
2022-04-06 16:15:01 -0700

    Remove extra comment.

--- ApplensBackend/Configuration/Startup.cs
+++ ApplensBackend/Configuration/Startup.cs
@@ -58,7 +58,6 @@ namespace AppLensV3
             services.AddSingleton<IObserverClientService, SupportObserverClientService>();
             services.AddSingleton<IDiagnosticClientService, DiagnosticClient>();
             services.AddSingleton<IGithubClientService, GithubClientService>();
-            //services.AddSingleton<IKustoQueryService, KustoQueryService>();
             services.AddSingleton<IKustoQueryService, KustoSDKClientQueryService>();
             services.AddSingleton<IOutageCommunicationService, OutageCommunicationService>();
             services.AddSingleton<ILocalDevelopmentClientService, LocalDevelopmentClientService>();
