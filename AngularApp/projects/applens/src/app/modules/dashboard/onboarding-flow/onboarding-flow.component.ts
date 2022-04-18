80e9b49905f06dd345c579ce7ef7360e2670f7c6
curibe <cristhianu@users.noreply.github.com>
2022-04-18 10:10:57 -0500

    typo in comment

--- AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
@@ -651,7 +651,7 @@ export class OnboardingFlowComponent implements OnInit {
   gistVersionChange() {
     var newGist;
     this.updateGistVersionOptions(this.pastGistEvent);
-    // updating the gistVersionOptions to taq with [in use] the new gist version being used and untag the previous one
+    // updating the gistVersionOptions to tag with [in use] the new gist version being used and untag the previous one
     this.gistVersionOptions.forEach(element => {
       element.key === this.selectedKey ? element.text = `${element.text} [in use]` : element.key === this.configuration['dependencies'][this.gistName] ? element.text = element.text.split(' [', 1)[0] : element.text = element.text;
     });
