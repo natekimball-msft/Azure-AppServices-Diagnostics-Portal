3e59ad9d0515872c12bfc6f4e48c54954f7a627c
Ajay Sharma <ajsharm@microsoft.com>
2022-04-22 17:10:05 -0700

    Some UI changes

--- AngularApp/projects/applens/src/app/modules/main/main/main.component.ts
+++ AngularApp/projects/applens/src/app/modules/main/main/main.component.ts
@@ -159,7 +159,10 @@ export class MainComponent implements OnInit {
         this.displayLoader = false;
       }
     },(err) => {
-        let errobj = JSON.parse(err.error);
+      let errormsg = err.error;
+      errormsg = errormsg.replace(/\\"/g, '"');
+      errormsg = errormsg.replace(/\"/g, '"');
+        let errobj = JSON.parse(errormsg);
         this.displayUserAccessError = true;
         this.userAccessErrorMessage = errobj.DetailText;
         this.displayLoader = false;
