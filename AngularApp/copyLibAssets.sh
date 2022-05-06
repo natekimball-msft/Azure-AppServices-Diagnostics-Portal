decc1f6c0d81b006a6e19953843ce4cee2b867e0
curibe <cristhianu@users.noreply.github.com>
2022-05-06 15:58:53 -0500

    Copying assests with sh script

---
+++ AngularApp/copyLibAssets.sh
@@ -0,0 +1,4 @@
+echo "-------------------------------------------------"
+echo "Copying assets to library dist folder."
+echo "-------------------------------------------------"
+cp -avr ./projects/diagnostic-data/src/lib/assets ./dist/diagnostic-data
