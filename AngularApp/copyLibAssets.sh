89531170324aa7f2434b53a20bf93b690da83383
curibe <cristhianu@users.noreply.github.com>
2022-05-06 16:07:11 -0500

    modifying path in sh script

--- AngularApp/copyLibAssets.sh
+++ AngularApp/copyLibAssets.sh
@@ -1,4 +1,4 @@
 echo "-------------------------------------------------"
 echo "Copying assets to library dist folder."
 echo "-------------------------------------------------"
-cp -avr ./projects/diagnostic-data/src/lib/assets ./dist/diagnostic-data
+cp -avr ./projects/diagnostic-data/src/lib/assets ./dist/diagnostic-data/assets
