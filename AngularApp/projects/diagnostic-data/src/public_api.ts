4580c92be05325566be332c3a2218e9b5cd55df3
curibe <cristhianu@users.noreply.github.com>
2022-05-06 11:49:49 -0500

    Removing vfs_fonts.js from public_api.ts

--- AngularApp/projects/diagnostic-data/src/public_api.ts
+++ AngularApp/projects/diagnostic-data/src/public_api.ts
@@ -58,5 +58,4 @@ export * from './lib/utilities/string-utilities';
 export * from './lib/components/step-views/step-view-lib';
 export * from "./lib/components/solution-orchestrator/solution-orchestrator.component";
 export * from './lib/utilities/uri-utilities';
-export * from './lib/assets/vfs_fonts.js';
 export * from './lib/utilities/resiliencyScoreReportHelper'
