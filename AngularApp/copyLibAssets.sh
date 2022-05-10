ad0697f84ccc7c4d58b0884965645884f30301f1
curibe <cristhianu@users.noreply.github.com>
2022-05-10 09:44:23 -0500

    Removing extra changes added to fix build error

--- AngularApp/copyLibAssets.sh
+++
@@ -1,4 +0,0 @@
-echo "-------------------------------------------------"
-echo "Copying assets to library dist folder."
-echo "-------------------------------------------------"
-cp -avr ./projects/diagnostic-data/src/lib/assets ./dist/diagnostic-data/assets
