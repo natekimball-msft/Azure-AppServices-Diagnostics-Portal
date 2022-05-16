c67bbabb79d1a5b14ab48af3f891189cdb128f53
curibe <cristhianu@users.noreply.github.com>
2022-05-16 16:29:29 -0500

    Adding downloadReport HTML and IsWindows logic

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-data/tab-data.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-data/tab-data.component.ts
@@ -1,10 +1,16 @@
-import { DetectorMetaData, DetectorResponse } from 'diagnostic-data';
+import { DetectorMetaData, DetectorResponse, ResiliencyScoreReportHelper } from 'diagnostic-data';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ApplensDiagnosticService } from '../../services/applens-diagnostic.service';
 import { DetectorControlService } from 'diagnostic-data';
 import { ApplensCommandBarService } from '../../services/applens-command-bar.service';
 import { ApplensGlobal } from 'projects/applens/src/app/applens-global';
+import { StartupService } from '../../../../shared/services/startup.service';
+import { ResourceService } from '../../../../shared/services/resource.service';
+import { ObserverService } from '../../../../shared/services/observer.service';
+
+
 
 @Component({
   selector: 'tab-data',
@@ -13,7 +19,7 @@ import { ApplensGlobal } from 'projects/applens/src/app/applens-global';
 })
 export class TabDataComponent implements OnInit {
 
-  constructor(private _route: ActivatedRoute, private _diagnosticApiService: ApplensDiagnosticService, private _detectorControlService: DetectorControlService,private _applensCommandBarService:ApplensCommandBarService,private _applensGlobal:ApplensGlobal) { }
+  constructor(private _route: ActivatedRoute, private _startupService: StartupService, public resourceService: ResourceService,  private _observerService: ObserverService, private _diagnosticApiService: DiagnosticApiService, private _applensDiagnosticApiService: ApplensDiagnosticService, private _detectorControlService: DetectorControlService,private _applensCommandBarService:ApplensCommandBarService,private _applensGlobal:ApplensGlobal) { }
 
   detectorResponse: DetectorResponse;
 
@@ -29,8 +35,43 @@ export class TabDataComponent implements OnInit {
   internalViewText: string = "Internal view";
   externalViewText: string = "Customer view";
 
-  ngOnInit() {
+  // Variables used by Download Report button
+  
+  resourceReady: Observable<any>;
+  resource: any;  
+  displayDownloadReportButton: boolean = false;
+  downloadReportText: string = "Get report";
+  downloadReportIcon: any = { iconName: 'Download' };
+  downloadReportButtonDisabled: boolean;
+  downloadReportFileName: string;
+  _isBetaSubscription: boolean = false;
+  generatedOn: string;
 
+  public _checkIsWindowsApp(): boolean {
+
+    let serviceInputs = this._startupService.getInputs();
+    this.resourceReady = this.resourceService.getCurrentResource();
+    this.resourceReady.subscribe(resource => {
+      if (resource) {  
+        this.resource = resource;
+        if (serviceInputs.resourceType.toString().toLowerCase() === 'microsoft.web/sites') {
+          this._observerService.getSiteRequestDetails(this.resource.SiteName, this.resource.InternalStampName).subscribe(siteInfo => {            
+            this.resource['IsWindows'] = siteInfo.details.islinux === false ? true : false;
+            if (this.resource['IsWindows'] === true) {
+              return true;
+            }
+            else {
+              return false;
+            }
+        })      
+        }
+      }
+    });
+    return false;
+  }    
+   
+
+  ngOnInit() {
     this._route.params.subscribe((params: Params) => {
       this.refresh();
     });
@@ -42,7 +83,8 @@ export class TabDataComponent implements OnInit {
         this._detectorControlService.setDetectorQueryParams("");
       }
 
-      this.analysisMode = this._route.snapshot.data['analysisMode'];
+      this.analysisMode = this._route.snapshot.data['analysisMode'];      
+      this.displayDownloadReportButton = this._route.firstChild.firstChild.firstChild.firstChild.firstChild.snapshot.params["detector"] === "ResiliencyScore" && this._checkIsWindowsApp() ? true: false;
     });
 
     if (this._detectorControlService.isInternalView){
@@ -51,11 +93,12 @@ export class TabDataComponent implements OnInit {
     else{
       this.internalExternalText = this.externalViewText;
     }
+    
   }
 
   refresh() {
     this.detector = this._route.snapshot.params['detector'];
-    this._diagnosticApiService.getDetectorMetaDataById(this.detector).subscribe(metaData => {
+    this._applensDiagnosticApiService.getDetectorMetaDataById(this.detector).subscribe(metaData => {
       if(metaData) {
         this._applensGlobal.updateHeader(metaData.name);
       }
