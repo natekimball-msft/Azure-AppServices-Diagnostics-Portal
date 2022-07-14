f2f708d7c853a8d7655873b1fdb0ace1becfdcf6
Pranav Rayudu <t-prayudu@microsoft.com>
2022-07-14 12:49:40 -0700

    fixed icons wrapping

--- AngularApp/projects/app-service-diagnostics/src/app/shared/components/tools/network-checks/apim-flows/data/constants.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/components/tools/network-checks/apim-flows/data/constants.ts
@@ -4,8 +4,10 @@ import { ResourceDescriptor, StatusStyles } from 'diagnostic-data';
 export const APIM_API_VERSION = "2021-12-01-preview";
 export const NETWORK_API_VERSION = "2021-08-01";
 
+let iconContainerStyles = "display: flex; align-items: center; padding-left: 1px; padding-bottom: 1px;";
+let iconStyles = "width: 17px; height: 17px; margin-right: 5px;";
 export let statusIconMarkdown = {
-    0: `<i class="${StatusStyles.HealthyIcon}" style="width: 17px; height: 17px;"></i> Success`,
-    1: `<i class="${StatusStyles.WarningIcon}" style="width: 17px; height: 17px;"></i> Warning`,
-    2: `<i class="${StatusStyles.CriticalIcon}" style="width: 17px; height: 17px;"></i> Error`, // fail
+    0: `<div style="${iconContainerStyles}"><i class="${StatusStyles.HealthyIcon}" style="${iconStyles}"></i> Success</div>`,
+    1: `<div style="${iconContainerStyles}"><i class="${StatusStyles.WarningIcon}" style="${iconStyles}"></i> Warning</div>`,
+    2: `<div style="${iconContainerStyles}"><i class="${StatusStyles.CriticalIcon}" style="${iconStyles}"></i> Error</div>`, // fail
 };
