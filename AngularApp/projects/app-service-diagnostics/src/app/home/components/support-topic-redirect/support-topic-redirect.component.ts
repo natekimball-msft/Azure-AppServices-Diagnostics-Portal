ebee6789b60563a577245c8380573ab52fbbf68b
Tuan Nguyen <tuanguye@microsoft.com>
2022-08-01 15:23:37 -0700

    Remove temp changes

--- AngularApp/projects/app-service-diagnostics/src/app/home/components/support-topic-redirect/support-topic-redirect.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/components/support-topic-redirect/support-topic-redirect.component.ts
@@ -16,26 +16,24 @@ import { ResourceService } from '../../../shared-v2/services/resource.service';
 export class SupportTopicRedirectComponent implements OnInit {
 
   constructor(private _activatedRoute: ActivatedRoute, private _router: Router, private _supportTopicService: GenericSupportTopicService, private _authService: AuthService,
-    private _notificationService: NotificationService, private portalKustoLogging: PortalKustoTelemetryService, private subscriptionPropertiesService: SubscriptionPropertiesService, private _riskAlertService: RiskAlertService, private _resourceService: ResourceService) { }
+    private _notificationService: NotificationService, private portalKustoLogging: PortalKustoTelemetryService, private subscriptionPropertiesService: SubscriptionPropertiesService, private _riskAlertService: RiskAlertService,private _resourceService:ResourceService) { }
 
   ngOnInit() {
     this._supportTopicService.getPathForSupportTopic(this._activatedRoute.snapshot.queryParams.supportTopicId, this._activatedRoute.snapshot.queryParams.pesId, this._activatedRoute.snapshot.queryParams.caseSubject, this._activatedRoute.snapshot.queryParams.sapSupportTopicId, this._activatedRoute.snapshot.queryParams.sapProductId).subscribe(res => {
-      this._router.navigate([`../${res.path}`], { relativeTo: this._activatedRoute, queryParams: res.queryParams }).catch(err => {
-        console.log(err)
-      });
+        this._router.navigate([`../${res.path}`], { relativeTo: this._activatedRoute, queryParams: res.queryParams });
 
-      // Discussed with the team and we decide to disable the notification for now
-      //   this._authService.getStartupInfo().subscribe(startupInfo => {
-      //     if (startupInfo.source && startupInfo.source.toLowerCase() == ('CaseSubmissionV2-NonContext').toLowerCase() && !startupInfo.isIFrameForCaseSubmissionSolution) {
-      //       const notification = new Notification('To continue with support request creation, please click on the "X" in the upper right corner.', null, MessageBarType.info, 'fa-info-circle', undefined, true);
-      //       this._notificationService.pushNotification(notification);
-      //     }
-      //   });
+    // Discussed with the team and we decide to disable the notification for now
+    //   this._authService.getStartupInfo().subscribe(startupInfo => {
+    //     if (startupInfo.source && startupInfo.source.toLowerCase() == ('CaseSubmissionV2-NonContext').toLowerCase() && !startupInfo.isIFrameForCaseSubmissionSolution) {
+    //       const notification = new Notification('To continue with support request creation, please click on the "X" in the upper right corner.', null, MessageBarType.info, 'fa-info-circle', undefined, true);
+    //       this._notificationService.pushNotification(notification);
+    //     }
+    //   });
 
-      this._riskAlertService.initRiskAlertsForArmResource(this._resourceService.resource.id);
-      this._riskAlertService.getRiskAlertNotificationResponse(true).subscribe((res) => {
+    this._riskAlertService.initRiskAlertsForArmResource(this._resourceService.resource.id);
+    this._riskAlertService.getRiskAlertNotificationResponse(true).subscribe((res)=>{
         this._riskAlertService.riskPanelContentsSub.next(this._riskAlertService.risksPanelContents);
-      });
+    });
 
     });
 
