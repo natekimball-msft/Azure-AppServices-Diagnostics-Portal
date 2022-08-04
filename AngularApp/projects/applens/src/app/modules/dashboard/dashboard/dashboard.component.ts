6d97fb14d7a7c7eca896768f843e79b060081300
Cristhian Uribe <curibe@microsoft.com>
2022-08-04 13:17:12 -0500

    Accessibility bug 2390 - Underline in Breadcrumb

--- AngularApp/projects/applens/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -71,6 +71,18 @@ export class DashboardComponent implements OnDestroy {
   breadcrumbStyles: IBreadcrumbProps['styles'] = {
     itemLink: {
       fontSize:"14px",
+      textDecoration: 'underline',
+      selectors: {
+        ':hover': {
+          textDecoration: 'underline',
+        },
+        ':focus': {
+          textDecoration: 'underline',
+        },
+        ':hover:focus': {
+          textDecoration: 'underline',
+        },
+      },
     },
   }
   breadcrumbTooltipHostProps: IBreadcrumbProps['tooltipHostProps'] = {
