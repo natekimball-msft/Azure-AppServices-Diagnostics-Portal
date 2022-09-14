ee1dfc50081c66a713c8d04521e2620b816e2b6c
D Donald <darreldonald@microsoft.com>
2022-09-14 10:09:58 -0700

    navigation protection (#1730)
    * navigation protection
    
    * remove comment
    
    * update navigation warning message and confirm method name
    
    * add missing semicolon

---
+++ AngularApp/projects/applens/src/app/modules/dashboard/develop-navigation-guard.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DevelopNavigationGuardService } from './develop-navigation-guard.service';
+
+describe('DevelopNavigationGuardService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: DevelopNavigationGuardService = TestBed.get(DevelopNavigationGuardService);
+    expect(service).toBeTruthy();
+  });
+});
