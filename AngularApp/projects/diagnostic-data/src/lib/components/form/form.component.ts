8fd203deda9272251edbd257f695e4f6901e65f5
rekhaswaminathan <46545195+rekhaswaminathan@users.noreply.github.com>
2022-11-28 16:02:17 -0800

    Fixed bug with forms as child detector (#1826)
    * Fixed bug with forms as child detector
    
    * Removed unused code

--- AngularApp/projects/diagnostic-data/src/lib/components/form/form.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/form/form.component.ts
@@ -127,7 +127,6 @@ export class FormComponent extends DataRenderBaseComponent {
   }
 
   OnSubmitFormAction(formId: any, buttonId: any) {
-
     let formToExecute = this.detectorForms.find(form => form.formId == formId);
     if (formToExecute != undefined) {
       // validate inputs. If there are validation errors displayed, do not proceed to execution
@@ -182,6 +181,7 @@ export class FormComponent extends DataRenderBaseComponent {
             formToExecute.errorMessage = 'Something went wrong while loading data';
           }));
       } else {
+        // Forms specific params
         let detectorParams = {
           'detectorId': this.detector,
           'fId': formId,
@@ -206,6 +206,13 @@ export class FormComponent extends DataRenderBaseComponent {
                       });
                 }
             }
+        });
+        this.activatedRoute.queryParams.subscribe(allRouteParams => {
+          for(let key of Object.keys(allRouteParams)) {
+            if(!detectorParams.hasOwnProperty(key)) {
+                detectorParams[key] = allRouteParams[key];
+            }
+        }
         });
         let detectorQueryParamsString = JSON.stringify(detectorParams);
         if (!this.isPublic) {
