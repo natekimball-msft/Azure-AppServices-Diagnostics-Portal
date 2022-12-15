e0ccc005080d9bda723a66e46f8a7b22b063b165
Darrel Donald <darreldonald@microsoft.com>
2022-12-15 13:14:03 -0800

    Update applens-dashboards-constant.ts

--- AngularApp/projects/applens/src/app/shared/utilities/applens-dashboards-constant.ts
+++ AngularApp/projects/applens/src/app/shared/utilities/applens-dashboards-constant.ts
@@ -10,5 +10,9 @@ export const applensDashboards: { name: string, link: string } [] = [
     {
         name: "AppLens Partner Ask DevOps Dashboard",
         link: "https://dev.azure.com/app-service-diagnostics-portal/app-service-diagnostics-portal/_dashboards/dashboard/08902cbd-1b75-4afa-8c14-6f63b60319d9"
+    },
+    {
+        name: "Genie Usage Dashboard",
+        link: "https://msit.powerbi.com/groups/b008a41c-b6a0-45c3-8b68-b8c2c4c6d53a/reports/ff2b7dd2-58c4-4f4b-a65c-009f7551f5ab/ReportSectiona185c8ea8ce49f0b697a?experience=power-bi"
     }
 ]
