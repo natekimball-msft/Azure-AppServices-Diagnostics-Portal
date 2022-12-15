b9540cb9864820ece1790ea77a88a6575489039e
D Donald <darreldonald@microsoft.com>
2022-12-15 13:42:31 -0800

    Darreldonald/dashboardlinks (#1862)
    * add dashboard links to landing page
    
    * Update applens-dashboards-constant.ts

---
+++ AngularApp/projects/applens/src/app/shared/utilities/applens-dashboards-constant.ts
@@ -0,0 +1,18 @@
+export const applensDashboards: { name: string, link: string } [] = [
+    {
+        name: "Self-help Intelligence Dashboard",
+        link: "https://msit.powerbi.com/groups/b008a41c-b6a0-45c3-8b68-b8c2c4c6d53a/reports/53842e93-c568-4002-9cc9-d1251511e718?ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&pbi_source=linkShare"
+    },
+    {
+        name: "Partner SLA Dashboard",
+        link: "https://msit.powerbi.com/groups/b008a41c-b6a0-45c3-8b68-b8c2c4c6d53a/reports/5f544785-e1a7-4abb-acbe-edb9c73d057f?ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&pbi_source=linkShare"
+    },
+    {
+        name: "AppLens Partner Ask DevOps Dashboard",
+        link: "https://dev.azure.com/app-service-diagnostics-portal/app-service-diagnostics-portal/_dashboards/dashboard/08902cbd-1b75-4afa-8c14-6f63b60319d9"
+    },
+    {
+        name: "Genie Usage Dashboard",
+        link: "https://msit.powerbi.com/groups/b008a41c-b6a0-45c3-8b68-b8c2c4c6d53a/reports/ff2b7dd2-58c4-4f4b-a65c-009f7551f5ab/ReportSectiona185c8ea8ce49f0b697a?experience=power-bi"
+    }
+]
