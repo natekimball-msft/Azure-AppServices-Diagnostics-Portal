356dcea1404f758ac7cf503d70490f773ec0528f
Ajay Sharma <ajsharm@microsoft.com>
2023-02-14 21:10:18 -0800

    Update generic open ai service

--- AngularApp/projects/diagnostic-data/src/lib/services/generic-openai.service.ts
+++ AngularApp/projects/diagnostic-data/src/lib/services/generic-openai.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { TextCompletionModel, OpenAIAPIResponse } from '../models/openai-data-models';
-import { Observable} from 'rxjs';
+import { Observable, Subject} from 'rxjs';
 
 @Injectable()
 export class GenericOpenAIService {
+  public isEnabled: boolean = false;
+  public IsEnabledUpdated: Subject<boolean> = new Subject<boolean>();
+
+  public CheckEnabled(): Subject<boolean> {
+    return this.IsEnabledUpdated;
+  }
 
   public generateTextCompletion(queryModel: TextCompletionModel, caching: boolean = true): Observable<OpenAIAPIResponse> {
     return null;
