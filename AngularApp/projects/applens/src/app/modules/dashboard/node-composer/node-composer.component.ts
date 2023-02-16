a78117352d577c9500f67aebd70664331982e05b
nmallick1 <nmallick@microsoft.com>
2023-02-16 11:39:10 -0800

    Adjust styling

--- AngularApp/projects/applens/src/app/modules/dashboard/node-composer/node-composer.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/node-composer/node-composer.component.ts
@@ -1,7 +1,8 @@
+import { Moment } from 'moment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DetectorMetaData, RenderingType } from 'diagnostic-data';
+import { DetectorControlService, DetectorMetaData, RenderingType } from 'diagnostic-data';
 import { IBasePickerProps, ITagPickerProps, ITagItemProps, ISuggestionModel, ITag, TagItem, IButtonStyles, IChoiceGroupOption, IDialogContentProps, IDialogProps, IDropdownOption, IDropdownProps, IIconProps, IPanelProps, IPersona, IPersonaProps, IPickerItemProps, IPivotProps, ITextFieldProps, MessageBarType, PanelType, SelectableOptionMenuItemType, TagItemSuggestion, IDropdown, ICalloutProps, ICheckboxStyleProps, ICheckboxProps, PivotItem } from 'office-ui-fabric-react';
 import { KeyValuePair } from 'projects/app-service-diagnostics/src/app/shared/models/portal';
 import { Observable, of } from 'rxjs';
@@ -9,6 +10,8 @@ import { ApplensGlobal } from '../../../applens-global';
 import { ComposerNodeModel, ITPromptSuggestionModel, NoCodeSupportedRenderingTypes } from '../models/detector-designer-models/node-models';
 import { DevelopMode } from '../onboarding-flow/onboarding-flow.component';
 import { ApplensDiagnosticService } from '../services/applens-diagnostic.service';
+import * as momentNs from 'moment';
+const moment = momentNs;
 
 @Component({
   selector: 'node-composer',
@@ -28,6 +31,79 @@ export class NodeComposer implements OnInit, OnDestroy {
   @Output() onDeleteClick = new EventEmitter<ComposerNodeModel>();
 
   private readonly TPROMPT_SUGGESTIONS_ENABLED:boolean = false;
+  startTime: Moment;
+  endTime: Moment;
+
+  // #region sample hardcoded data
+
+  public sampleTestDataset =  [
+    {
+      "table": {
+          "tableName": "Table_0",
+          "columns": [
+              {
+                  "columnName": "EventStampName",
+                  "dataType": "String",
+                  "columnType": null
+              },
+              {
+                  "columnName": "RoleInstance",
+                  "dataType": "String",
+                  "columnType": null
+              },
+              {
+                  "columnName": "Details",
+                  "dataType": "String",
+                  "columnType": null
+              },
+              {
+                  "columnName": "LatestBuildVersion",
+                  "dataType": "String",
+                  "columnType": null
+              }
+          ],
+          "rows": [
+              [
+                  "waws-prod-bay-111",
+                  "dw0SmallDedicatedWebWorkerRole_IN_10762",
+                  "10.10.0.232",
+                  "99.0.10.793"
+              ],
+              [
+                  "waws-prod-bay-111",
+                  "dw1SmallDedicatedWebWorkerRole_IN_5888",
+                  "10.11.0.147",
+                  "99.0.10.793"
+              ],
+              [
+                  "waws-prod-bay-111",
+                  "dw1SmallDedicatedWebWorkerRole_IN_2356",
+                  "10.11.1.209",
+                  "99.0.10.793"
+              ],
+              [
+                  "waws-prod-bay-111",
+                  "dw1SmallDedicatedWebWorkerRole_IN_8158",
+                  "10.11.0.57",
+                  "99.0.10.793"
+              ],
+              [
+                  "waws-prod-bay-111",
+                  "pd0LargeDedicatedWebWorkerRole_IN_25631",
+                  "10.20.64.39",
+                  "99.0.10.793"
+              ]
+          ]
+      },
+      "renderingProperties": {
+          "type": 1,
+          "title": "Sample Table",
+          "description": "Some description here",
+          "isVisible": true
+      }
+    }];
+// #endregion sample hardcoded data
+
 
   //#region Fabric element styles
   fabTextFieldStyleNoStretch: ITextFieldProps["styles"] = {
@@ -124,12 +200,16 @@ export class NodeComposer implements OnInit, OnDestroy {
       });
     }
 
+    this.startTime = this._detectorControl.startTime;
+    this.endTime = this._detectorControl.endTime;
+
     this.pivotSelectedKey = this.nodeModel.id + '_Query';
 
     this.initMonacoEditorOptions();
   }
 
-  constructor(private _applensGlobal: ApplensGlobal, private _activatedRoute: ActivatedRoute, private _router: Router, public diagnosticApiService: ApplensDiagnosticService,private _httpClient: HttpClient ) {
+  constructor(private _applensGlobal: ApplensGlobal, private _activatedRoute: ActivatedRoute, private _router: Router,
+    private _detectorControl: DetectorControlService, public diagnosticApiService: ApplensDiagnosticService,private _httpClient: HttpClient ) {
     this._applensGlobal.updateHeader('');    
   }
 
@@ -240,7 +320,6 @@ export class NodeComposer implements OnInit, OnDestroy {
 
     this.nodeModel.editorRef = editor;
   }
-
 }
 
 export interface INodeModelChangeEventProps {
