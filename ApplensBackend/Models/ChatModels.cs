b5a5d84c21c0190f2702421bebde95287d0713fa
nmallick1 <nmallick@microsoft.com>
2023-06-09 12:13:02 -0700

    Add model to return chat response

--- ApplensBackend/Models/ChatModels.cs
+++ ApplensBackend/Models/ChatModels.cs
@@ -1,4 +1,5 @@
-﻿using Azure.AI.OpenAI;
+﻿using Azure;
+using Azure.AI.OpenAI;
 using SendGrid.Helpers.Mail;
 
 namespace AppLensV3.Models
@@ -34,6 +35,42 @@ namespace AppLensV3.Models
         {
             Content = content;
             FinishReason = finishReason;
+        }
+    }
+
+    public class ChatResponse
+    {
+        /// <summary>
+        /// Gets or sets response to show on the UI.
+        /// </summary>
+        public string Text { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets a value indicating whether the response is truncated and if more is to follow.
+        /// </summary>
+        public bool Truncated
+        {
+            get => FinishReason.Equals("length", System.StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Gets or sets a description of how evaluation of this chat ended.
+        /// </summary>
+        public string FinishReason { get; set; } = string.Empty;
+
+        public ChatResponse(string chatResponse, string finishReason)
+        {
+            Text = chatResponse ?? string.Empty;
+            FinishReason = finishReason ?? string.Empty;
+        }
+
+        public ChatResponse(Response<ChatCompletions> chatCompletionResponse)
+        {
+            if (chatCompletionResponse?.Value?.Choices?.Count > 0)
+            {
+                Text = chatCompletionResponse.Value.Choices[0].Message?.Content ?? string.Empty;
+                FinishReason = chatCompletionResponse.Value.Choices[0].FinishReason ?? string.Empty;
+            }
         }
     }
 }
